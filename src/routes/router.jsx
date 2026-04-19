8157244c589cb2b070fba5c45d1c2f55c502ba76
Nur Mohammod <nurm98371@gmail.com>
2026-04-20 05:45:42 +0600

    ADD

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -29,7 +29,7 @@ export const router = createBrowserRouter([
         element: <Profile></Profile>,
       },
      {
-        path: "login", // ❗ remove leading slash
+        path: "login",
         element: <Login />,
       },
       {
