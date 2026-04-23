f8364d962dae09de41cafbe93238e1b14da24434
Biplob Roy Chowdhury Likhon <156028597+Biplob-Roy-Chowdhury@users.noreply.github.com>
2026-04-23 11:25:00 +0600

    Clean up Firebase initialization code
    Removed unnecessary comments and improved code formatting.

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -3,11 +3,9 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; // <-- Correct package
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
 
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
@@ -18,7 +16,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_measurementId,
 };
 
-// Initialize Firebase
 
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
