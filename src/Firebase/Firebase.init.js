10e6e37c67aa6150c85c1239b8fa93d2a30ac584
Biplob Roy Chowdhury Likhon <156028597+Biplob-Roy-Chowdhury@users.noreply.github.com>
2026-04-23 11:24:23 +0600

    Clean up Firebase.init.js comments
    Removed unnecessary comments and added a note to include SDKs for Firebase products.

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -1,10 +1,10 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; // <-- Correct package
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
